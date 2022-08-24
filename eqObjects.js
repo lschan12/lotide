2ccdc8651b763246fe1244332bce39cc963035fc
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-24 14:10:18 -0700

    implemented eqobjects function that compares two objects and outputs a true or false depending on if they have the same keys and values

---
+++ eqObjects.js
@@ -0,0 +1,53 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let result = true;
+  const findLongestArray = (arrayOne, arrayTwo) => {
+    let combinedArray = [];
+    combinedArray.push(arrayOne, arrayTwo);
+    let longestArray = combinedArray.reduce((a, b) => a.length > b.length ? a : b);
+    return longestArray;
+  };
+  let longestArray = (findLongestArray(arrayOne, arrayTwo));
+  for (let i = 0; i < longestArray.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      result = true;
+    } else {
+      result = false;
+      break;
+    }
+  }
+  return result;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let keys of Object.keys(object1)) {
+    if (Array.isArray(object1[keys]) && Array.isArray(object2[keys])) {
+      if (!eqArrays(object1[keys], object2[keys])) {
+        return false;
+      }
+    } else if (object1[keys] !== object2[keys]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
+
