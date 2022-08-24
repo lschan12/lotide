ec7aa2072c68e83e2e0a86b9f5b5e008dbbb626c
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-24 12:08:35 -0700

    Implemented a letter position function that receives a string and outputs an object with each string and its indecies as keys and values respectively.

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  let result = true;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      result = true;
+    } else {
+      result = false;
+      break;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  let output = "";
+  if (eqArrays(arrayOne, arrayTwo)) {
+    output += `✅✅✅ These two arrays are equal!`;
+  } else {
+    output += `🛑🛑🛑 These two arrays are not equal!`;
+  }
+  console.log(output);
+};
+
+
+const letterPositions = function(sentence) {
+  let result = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (result[sentence[i]]) {
+        result[sentence[i]].push(i);
+      } else {
+        result[sentence[i]] = [i];
+      }
+    }
+  }
+  return result;
+};
+
+assertArraysEqual(letterPositions("lighthouse in the house")["h"], [3, 5, 15, 18]);
