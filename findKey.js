8a40b54ac53c1127b5399ddec5ca29859b628067
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-24 22:26:55 -0700

    implemented a function that receives an object and a callback function and returns the object's first key that the callback function returns true.

---
+++ findKey.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let o in object) {
+    if (callback(object[o])) {
+      return o;
+    }
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
+
+assertEqual(findKey({
+  GreatSword: {damage: 50},
+  BattleAxe: {damage: 60},
+  LongSword: {damage: 30},
+  Dagger: {damage: 10},
+}, x => x.damage < 40), "LongSword");
+
