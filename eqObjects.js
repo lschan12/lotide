822a1b76a42669a2215400ff282bdbfe39f72e53
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-26 12:25:24 -0700

    implemented eqObjects to copmare nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -35,7 +35,8 @@ const eqObjects = function(object1, object2) {
       if (!eqArrays(object1[keys], object2[keys])) {
         return false;
       }
-    } else if (object1[keys] !== object2[keys]) {
+    }
+    if (!eqObjects(object1[keys], object2[keys])) {
       return false;
     }
   }
@@ -44,10 +45,15 @@ const eqObjects = function(object1, object2) {
 
 
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true); // => true
 
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false); // => false
+
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { y: 0, z: 1 }, b: 2 })); // => false
