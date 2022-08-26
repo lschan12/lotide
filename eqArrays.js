8f3d81b24d271cb41c093c44c20dfb6a450894d4
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-26 13:28:27 -0700

    simplified the code to not have to find longest array, and instead just return false if array lengths are not the same.

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,24 @@
 const eqArrays = function(arrayOne, arrayTwo) {
-  let result = true;
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
   for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] === arrayTwo[i]) {
-      result = true;
-    } else {
-      result = false;
-      break;
+    if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
+      eqArrays(arrayOne[i], arrayTwo[i]);
+    }
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
     }
   }
-  return result;
+  return true;
 };
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1]))// => false
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))// => false
+
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
 
