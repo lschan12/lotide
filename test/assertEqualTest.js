0ded09c9f2262f7230e629a473b8814741cd762c
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-28 21:46:46 -0700

    removed irrelevant code from assertEqualTest.js

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,38 +1,7 @@
 const assertEqual = require("../assertEqual");
-const head = require("../head");
-const tail = require("../tail");
-const eqArrays = require("../eqArrays");
-const assertArraysEqual = require("../assertArraysEqual");
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Hello", "Hello");
 assertEqual(2, 4);
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), 2);
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);// => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);// => false
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
-
-assertArraysEqual([1, 2, 3], [3, 2, 1], false); // => false
-assertArraysEqual([1, 2, 3], [1, 2, 3], true); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"], true); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3], false); // => false
