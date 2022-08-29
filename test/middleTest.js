3a45bb1854de42848ce897c206070f2b1504c3a0
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-28 22:35:24 -0700

    refactored the test files to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
+const assert = require("chai").assert;
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("should return [] when there is only 1 item in the array", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("should return [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("should return [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("should return [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("should return [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,20 @@
+const assert = require("chai").assert;
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+describe("#tail", () => {
+  it("returns a length of 3 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+  it("returns `Lighthouse' for ['Hello', 'Lighthouse', 'Labs'] for result[0] after tail has been used on original array", () => {
+    const words = ["Hello", "Lighthouse", "Labs"];
+    let result = tail(words);
+    assert.strictEqual(result[0], "Lighthouse");
+  });
+  it("returns `Labs' for ['Hello', 'Lighthouse', 'Labs'] for result[1] after tail has been used on original array", () => {
+    const words = ["Hello", "Lighthouse", "Labs"];
+    let result = tail(words);
+    assert.strictEqual(result[1], "Labs");
+  });
+});
