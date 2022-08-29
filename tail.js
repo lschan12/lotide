48636043887e20ab26d0ce9bf13ac9daed48f3f6
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-28 21:18:25 -0700

    refactored tail.js using export.modules and moving test codes into the test file

--- tail.js
+++ tail.js
@@ -1,23 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const tail = function(array) {
   let output = array.slice(1, array.length);
   return output;
 };
+module.exports = tail;
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
 
-// Test Case: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,6 @@
 const assertEqual = require("../assertEqual");
 const head = require("../head");
+const tail = require("../tail");
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
@@ -10,3 +11,12 @@ assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([1]), 1);
 assertEqual(head([]), 2);
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
