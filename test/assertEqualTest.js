e03fdb846829a59104fee43fd8c615b540677a55
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-28 23:40:10 -0700

    implemented unit test for assertEqualTest and refactered assertEqual.js file to return true or false rather than a console log

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,23 @@
+const assert = require("chai").assert;
 const assertEqual = require("../assertEqual");
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Hello", "Hello");
-assertEqual(2, 4);
+describe("#assertEqual", () => {
+  it("should return false for 'Lighthouse Labs' and 'Bootcamp'", () => {
+    assert.isFalse(assertEqual("Lighthouse Labs", "Bootcamp"));
+  });
+  it("should return true for 1 and 1", () => {
+    assert.isTrue(assertEqual(1, 1));
+  });
+  it("should return true for 'Hello' and 'Hello'", () => {
+    assert.isTrue(assertEqual("Hello", "Hello"));
+  });
+  it("should return false for 2 and 4'", () => {
+    assert.isFalse(assertEqual(2, 4));
+  });
+});
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("Hello", "Hello");
+// assertEqual(2, 4);
 
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,27 @@
+const assert = require("chai").assert;
 const eqArrays = require("../eqArrays");
-const assertEqual = require("../assertEqual");
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);// => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);// => false
+describe("#eqArray", () => {
+  it("should return true when comparing [1, 2, 3] and [1, 2, 3]", () => {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+  });
+  it("should return false when comparing [1, 2, 3] and [3, 2, 1]", () => {
+    assert.isFalse(eqArrays([1, 2, 3], [3, 2, 1]));
+  });
+  it("should return false when comparing ['1', '2', '3'] and ['3', '2', '1']", () => {
+    assert.isFalse(eqArrays(['1', '2', '3'], ['3', '2', '1']));
+  });
+  it("should return true when comparing ['1', '2', '3'] and ['1', '2', '3']", () => {
+    assert.isTrue(eqArrays(['1', '2', '3'], ['1', '2', '3']));
+  });
+  it("should return true when comparing [[2, 3], [4]] and [[2, 3], [4]]", () => {
+    assert.isTrue(eqArrays([[2, 3], [4]], [[2, 3], [4]]));
+  });
+  it("should return false when comparing [[2, 3], [4]] and [[2, 3], [4, 5]]", () => {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]));
+  });
+  it("should return false when comparing [[2, 3], [4]] and [[2, 3], [4, 5]]", () => {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], 4]));
+  });
+});
+
